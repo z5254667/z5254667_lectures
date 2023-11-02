c81132b7e13c2ea53858c2d1792218e6827875aa
John Salvaris <z5254667@ad.unsw.edu.au>
2023-11-02 11:38:53 +1100

    Initial commit

---
+++ toolkit_config.py
@@ -0,0 +1,9 @@
+""" toolkit_config.py
+
+Project configuration file
+"""
+import os
+
+
+PRJDIR = '/Users/johnsalvaris/Documents/University/Y5 T3/FINS3646/toolkit'
+DATADIR = os.path.join(PRJDIR, 'data')
